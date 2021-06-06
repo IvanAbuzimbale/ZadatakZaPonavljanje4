d32c1a0fc2def90ef1f0da1259186ac612dd494e
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-06-06 16:00:56 +0200

    Add files via upload

---
+++ ZadatakZaPonavljanje4/ZadatakZaPonavljanje4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var broj = new Random();
+            List<int> brojevi = new List<int>();
+            string[] igraci = new string[32];
+            for (int i = 0; i < 32; i++)
+            {
+                igraci[i] = "Tenisač " + (i+1);
+            }
+            for (int i = 0, j = 0; i < 16; i++)
+            {
+                Console.WriteLine("Runda broj " + (i + 1) + ": " + igraci[j] + " je protiv " + igraci[j + 1]);
+                j = j + 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
